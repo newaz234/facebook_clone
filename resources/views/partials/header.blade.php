5ff329048924a7b98b5c8b4dea15cfbbbfec43f6
Newaz Mohammad hamim <newazmohammedhamim@gmail.com>
2025-10-24 23:47:57 +0600

    modified layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,25 +8,17 @@
                 <input type="text" placeholder="Search Facebook">
             </div>
         </div>
-        
         <div class="nav-icons">
             <div class="nav-icon active">
                 <i class="fas fa-home" onclick="window.location.href='{{ route('hompage') }}'"></i>
             </div>
             <div class="nav-icon">
-                <i class="fas fa-users"></i>
-            </div>
-            <div class="nav-icon">
-                <i class="fas fa-store"></i>
+             <i class="fa-brands fa-facebook-messenger" onclick="window.location.href='{{ route('messages.index') }}'"></i>
             </div>
             <div class="nav-icon">
                 <i class="fas fa-user-group" onclick="window.location.href='{{ route('friends.index') }}'"></i>
             </div>
-            <div class="nav-icon">
-                <i class="fas fa-gamepad"></i>
-            </div>
-        </div>
-        
+        </div> 
         <div class="user-profile" onclick="window.location.href='{{ route('profile') }}'">
             <img src="{{ asset('storage/' . $user->image) }}" alt="Profile">
             <span>{{$user->surname}}</span>
